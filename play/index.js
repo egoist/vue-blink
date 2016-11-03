24b7b5c74ef26b78dda69e77b1b1c5eac9dc3c96
EGOIST <0x142857@gmail.com>
2016-11-03 14:23:03 +0800

    add example

--- play/index.js
+++ play/index.js
@@ -1,8 +1,16 @@
 import {play} from 'vue-play'
 import Blink from '../src'
 
-play('Button', module)
-  .add('text', h => h(Blink, 'ga hahahahah'))
-  .add('image', h => <Blink>
+play('Blink', module)
+  .add('text', {
+    example: '<Blink>ga hahahahah</Blink>',
+    render: h => h(Blink, 'ga hahahahah')
+  })
+  .add('image', {
+    example: `<Blink
+  <img src="http://ww4.sinaimg.cn/large/a15b4afegw1f9ew74qyc8j206103ea9v" />
+</Blink>`,
+    render: h => <Blink>
     <img src="http://ww4.sinaimg.cn/large/a15b4afegw1f9ew74qyc8j206103ea9v" />
-  </Blink>)
+  </Blink>
+  })
