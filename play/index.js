c247316caec412d8d58fa26ee2c74b057261c92b
EGOIST <0x142857@gmail.com>
2016-11-03 14:24:31 +0800

    typo

--- play/index.js
+++ play/index.js
@@ -7,7 +7,7 @@ play('Blink', module)
     render: h => h(Blink, 'ga hahahahah')
   })
   .add('image', {
-    example: `<Blink
+    example: `<Blink>
   <img src="http://ww4.sinaimg.cn/large/a15b4afegw1f9ew74qyc8j206103ea9v" />
 </Blink>`,
     render: h => <Blink>
